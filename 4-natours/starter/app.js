75ddcef322edeea25a7c51276d17089744dd64fe
Mic <micangel@mail.ru>
2019-11-06 11:30:57 +0300

    Before MongoDB

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,10 +7,13 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1) MIDDLEWARE
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
+//1) MIDDLEWARE
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello from middleware!!!');
--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -74,3 +74,14 @@ exports.deleteTour = (req, res) => {
         data: null
     });
 };
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        })
+    }
+
+    next();
+}
