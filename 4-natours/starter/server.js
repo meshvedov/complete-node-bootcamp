804d54e651a2e9c98c803e5c0eebdf980bf41f64
Mic <micangel@mail.ru>
2019-11-14 09:59:25 +0300

    UnhandledRejection and UncaughtExeption error into server.js

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+	console.log('UNCAUGHT EXCEPTION !!!');
+	console.log(err.name, err.message);
+	process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -21,6 +27,14 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+	console.log(err.name, err.message);
+	console.log('UNHANDLED REJECTION !!!');
+	server.close(err => {
+		process.exit(1);
+	});
 });
