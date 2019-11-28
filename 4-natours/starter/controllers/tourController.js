395fb8829bb545bed70d654ca4a8a9a81c548d74
Mic <micangel@mail.ru>
2019-11-28 11:28:40 +0300

    Factories for get

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -35,69 +35,68 @@ exports.aliasTopTours = (req, res, next) => {
 	next();
 };
 
-exports.getAllTours = catchAsync(async (req, res, next) => {
-	const features = new APIFeatures(Tour.find(), req.query)
-		.filter()
-		.sort()
-		.limitFields()
-		.paginate();
-	const tours = await features.query;
+// exports.getAllTours = catchAsync(async (req, res, next) => {
+// 	const features = new APIFeatures(Tour.find(), req.query)
+// 		.filter()
+// 		.sort()
+// 		.limitFields()
+// 		.paginate();
+// 	const tours = await features.query;
 
-	res.status(200).json({
-		status: 'success',
-		results: tours.length,
-		data: {
-			tours
-		}
-	});
-	// try {
-	// 	//Build Query
-	// 	//1) Filtering
+// 	res.status(200).json({
+// 		status: 'success',
+// 		results: tours.length,
+// 		data: {
+// 			tours
+// 		}
+// 	});
+// try {
+// 	//Build Query
+// 	//1) Filtering
 
-	// 	//3) Sorting
+// 	//3) Sorting
 
-	// 	//3) Field Limiting
-	// 	// if (req.query.fields) {
-	// 	// 	const fields = req.query.fields.split(',').join(' ');
-	// 	// 	query = query.select(fields);
-	// 	// } else {
-	// 	// 	query = query.select('-__v');
-	// 	// }
+// 	//3) Field Limiting
+// 	// if (req.query.fields) {
+// 	// 	const fields = req.query.fields.split(',').join(' ');
+// 	// 	query = query.select(fields);
+// 	// } else {
+// 	// 	query = query.select('-__v');
+// 	// }
 
-	// 	//4) Pagination
-	// 	//?page=2&limit=10
-	// 	// const page = req.query.page * 1 || 1;
-	// 	// const limit = req.query.limit * 1 || 100;
-	// 	// const skip = (page - 1) * limit;
-	// 	// query = query.skip(skip).limit(limit);
+// 	//4) Pagination
+// 	//?page=2&limit=10
+// 	// const page = req.query.page * 1 || 1;
+// 	// const limit = req.query.limit * 1 || 100;
+// 	// const skip = (page - 1) * limit;
+// 	// query = query.skip(skip).limit(limit);
 
-	// 	// if (req.query.page) {
-	// 	// 	const numTours = await Tour.countDocuments();
-	// 	// 	if (skip > numTours) throw new Error('This page does not exist');
-	// 	// }
+// 	// if (req.query.page) {
+// 	// 	const numTours = await Tour.countDocuments();
+// 	// 	if (skip > numTours) throw new Error('This page does not exist');
+// 	// }
 
-	// 	//Execute Query
-	// 	const features = new APIFeatures(Tour.find(), req.query)
-	// 		.filter()
-	// 		.sort()
-	// 		.limitFields()
-	// 		.paginate();
-	// 	const tours = await features.query;
+// 	//Execute Query
+// 	const features = new APIFeatures(Tour.find(), req.query)
+// 		.filter()
+// 		.sort()
+// 		.limitFields()
+// 		.paginate();
+// 	const tours = await features.query;
 
-	// 	res.status(200).json({
-	// 		status: 'success',
-	// 		results: tours.length,
-	// 		data: {
-	// 			tours
-	// 		}
-	// 	});
-	// } catch (error) {
-	// 	res.status(404).json({
-	// 		status: 'fail',
-	// 		message: error
-	// 	});
-	// }
-});
+// 	res.status(200).json({
+// 		status: 'success',
+// 		results: tours.length,
+// 		data: {
+// 			tours
+// 		}
+// 	});
+// } catch (error) {
+// 	res.status(404).json({
+// 		status: 'fail',
+// 		message: error
+// 	});
+// }
 
 // exports.getTour = (req, res) => {
 // 	console.log(req.params);
@@ -119,104 +118,13 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 // 	    }
 // 	});
 // };
+exports.getAllTours = factory.getAll(Tour);
 
-exports.getTour = catchAsync(async (req, res, next) => {
-	const tour = await Tour.findById(req.params.id).populate('reviews');
+exports.getTour = factory.getOne(Tour, { path: 'reviews' });
 
-	if (!tour) {
-		return next(new AppError('No tour found with that ID', 404));
-	}
+exports.createTour = factory.createOne(Tour);
 
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour
-		}
-	});
-	// try {
-	// 	const tour = await Tour.findById(req.params.id);
-	// 	res.status(200).json({
-	// 		status: 'success',
-	// 		data: {
-	// 			tour
-	// 		}
-	// 	});
-	// } catch (err) {
-	// 	res.status(404).json({
-	// 		status: 'fail',
-	// 		message: err
-	// 	});
-	// }
-});
-
-// exports.updateTour = (req, res) => {
-// 	res.status(200).json({
-// 		status: 'success',
-// 		data: {
-// 			tour: '<Updating success...>'
-// 		}
-// 	});
-// };
-
-exports.createTour = catchAsync(async (req, res, next) => {
-	const newTour = await Tour.create(req.body);
-
-	res.status(201).json({
-		status: 'success',
-		data: {
-			tour: newTour
-		}
-	});
-	// try {
-	// 	const newTour = await Tour.create(req.body);
-
-	// 	res.status(201).json({
-	// 		status: 'success',
-	// 		data: {
-	// 			tour: newTour
-	// 		}
-	// 	});
-	// } catch (err) {
-	// 	res.status(400).json({
-	// 		status: 'fail',
-	// 		message: 'Invalid data sent!'
-	// 	});
-	// }
-});
-
-exports.updateTour = catchAsync(async (req, res, next) => {
-	const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
-		runValidators: true
-	});
-
-	if (!tour) {
-		return next(new AppError('No tour found with that ID', 404));
-	}
-	res.status(200).json({
-		status: 'success',
-		data: {
-			tour
-		}
-	});
-	// try {
-	// 	const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-	// 		new: true,
-	// 		runValidators: true
-	// 	});
-	// 	res.status(200).json({
-	// 		status: 'success',
-	// 		data: {
-	// 			tour
-	// 		}
-	// 	});
-	// } catch (err) {
-	// 	res.status(404).json({
-	// 		status: 'fail',
-	// 		message: err
-	// 	});
-	// }
-});
+exports.updateTour = factory.updateOne(Tour);
 
 // exports.createTour = (req, res) => {
 //     const newId = tours[tours.length - 1].id + 1;
