c9008a20fed9412d4ce22e97012b2809ffbb8a06
Mic <micangel@mail.ru>
2019-11-29 15:53:53 +0300

    Authorization added in user,tour,reviewRoutes

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -7,9 +7,11 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+//Protect all routes after this middleware
+router.use(authController.protect);
 
 router.patch(
 	'/updateMyPassword',
@@ -26,6 +28,8 @@ router.get(
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
 
 router
 	.route('/')
